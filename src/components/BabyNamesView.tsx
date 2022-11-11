a11b7b9359020871f34cd9ae02fce5e3d3ebc6b0
grace k <grace.kuperman5472@gmail.com>
2022-11-11 15:41:07 +0000

    prettier a third time react line removed

--- src/components/BabyNamesView.tsx
+++ src/components/BabyNamesView.tsx
@@ -1,8 +1,6 @@
 import { NameView } from "./NameView";
 import { myNames, OneName } from "../BabyNamesData";
 
-
-
 export function BabyNamesView(): JSX.Element {
   return (
     <div>
@@ -10,10 +8,10 @@ export function BabyNamesView(): JSX.Element {
       Baby Names View
       {myNames.map((nameItem: OneName) => (
         <NameView onename={nameItem} key={nameItem.id} />
-        ))}
+      ))}
     </div>
   );
 }
 // (nameitem: onename) is the paramter list for the map function
-//line 10 curlies are jsx expressions 
-//name item is just a ref to a variable 
+//line 10 curlies are jsx expressions
+//name item is just a ref to a variable
--- src/components/NameView.tsx
+++ src/components/NameView.tsx
@@ -1,4 +1,3 @@
-import react from "react";
 import { OneName } from "../BabyNamesData";
 
 interface NameViewProps {
