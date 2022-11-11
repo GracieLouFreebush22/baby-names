b72f84f5aa90537e916797d05266cf166efdd806
grace k <grace.kuperman5472@gmail.com>
2022-11-11 15:37:22 +0000

    notes from neill added

--- src/components/BabyNamesView.tsx
+++ src/components/BabyNamesView.tsx
@@ -1,6 +1,8 @@
 import { NameView } from "./NameView";
 import { myNames, OneName } from "../BabyNamesData";
 
+
+
 export function BabyNamesView(): JSX.Element {
   return (
     <div>
@@ -8,7 +10,10 @@ export function BabyNamesView(): JSX.Element {
       Baby Names View
       {myNames.map((nameItem: OneName) => (
         <NameView onename={nameItem} key={nameItem.id} />
-      ))}
+        ))}
     </div>
   );
 }
+// (nameitem: onename) is the paramter list for the map function
+//line 10 curlies are jsx expressions 
+//name item is just a ref to a variable 
