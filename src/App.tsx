c7c30237bc05b4dc513d1a85c67a6b8d2f9f472f
grace k <grace.kuperman5472@gmail.com>
2022-11-11 16:17:11 +0000

    app header added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BabyNamesView } from "./components/BabyNamesView";
-//import {PlaceView} from "./components/PlaceView"
+import { AppHeader } from "./components/AppHeader";
 import "./style.css";
 
 function App(): JSX.Element {
   return (
     <div>
       <h1> Grace's Baby Names Website</h1>
+      <AppHeader/>
       <div>
         <BabyNamesView />
       </div>
