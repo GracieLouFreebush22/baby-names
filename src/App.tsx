532109af00b1058a152b5bfa8af59d423a251dcf
grace k <grace.kuperman5472@gmail.com>
2022-11-14 13:37:02 +0000

    fixed ug

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BabyNamesView } from "./components/BabyNamesView";
 import { AppHeader } from "./components/AppHeader";
 import "./style.css";
+import { KeyboardControlledInput } from "./components/KeyboardControlledInput";
 
 function App(): JSX.Element {
   return (
     <div>
       <h1> Grace's Baby Names Website</h1>
-      <AppHeader/>
+      <AppHeader />
+      <KeyboardControlledInput/>
       <div>
         <BabyNamesView />
+        
       </div>
     </div>
   );
