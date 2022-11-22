c5c36093caaf6b3d71ca9c440fa5c4ce0c351bd7
grace k <grace.kuperman5472@gmail.com>
2022-11-22 12:42:53 +0000

    case sensitivity working

--- src/components/BabyNamesView.tsx
+++ src/components/BabyNamesView.tsx
@@ -1,6 +1,5 @@
 import { NameView } from "./NameView";
 import { OneName, mySortedNames } from "../BabyNamesData";
-//import {searchMessage} from "../App";
 
 export interface NameViewProps {
   onename: OneName;
@@ -10,18 +9,21 @@ interface BabyNamesViewProps {
   searchmessage: string;
 }
 
-export function BabyNamesView({
-  searchmessage,
-}: BabyNamesViewProps): JSX.Element {
+export function BabyNamesView({searchmessage,}: BabyNamesViewProps): JSX.Element {
   const matchingBabyNames = findMatchingBabyNames(searchmessage, mySortedNames);
+  
 
   return (
+    <div>
+
+    
     <div className="names-view">
       <ul>
         {matchingBabyNames.map((nameItem: OneName) => (
           <NameView onename={nameItem} key={nameItem.id} />
         ))}
       </ul>
+    </div>
     </div>
   );
 }
@@ -33,7 +35,7 @@ function findMatchingBabyNames(
   const resultNames: OneName[] = [];
   //looking at prop of array instead of prop of a object
   for (const item of mySortedNames) {
-    if (item.name.includes(searchMessage)) {
+    if (item.name.toLowerCase().includes(searchMessage)) {
       resultNames.push(item);
     }
   }
